e97407962fdfbfec79233a7157bc8dade7758ba0
Petru <smart__petea@mail.ru>
2021-05-31 22:31:59 +0300

    fig3_6

--- ch3/fig3_6/src/main.rs
+++ ch3/fig3_6/src/main.rs
@@ -1,2 +1,30 @@
+#[macro_use]
+extern crate scan_fmt;
+
+use std::io::{Write, stdout};
+
 fn main() {
+    let mut counter: i32;
+    let mut total: i32;
+
+    total = 0;
+    counter = 1;
+
+    while counter <= 10 {
+        print!("Enter grade: ");
+        if let Err(e) = stdout().flush() {
+            panic!("{}", e)
+        }
+
+        if let Ok(grade) =  scanln_fmt!("{}", i32) {
+            total = total + grade;
+        } else {
+            panic!("Wrong value introduced");
+        }
+
+        counter = counter + 1;
+    }
+
+    let average = total / 10;
+    println!("Class average is {}", average)
 }
